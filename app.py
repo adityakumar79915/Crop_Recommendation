b9b9dc30e8abc552d947e3c32b6611d0a1c986c7
adityakumar79915 <adityakumar79915@gmail.com>
2024-07-01 09:12:51 +0530

    updated

--- app.py
+++ app.py
@@ -32,13 +32,13 @@ def predict():
     sc_mx_features = sc.transform(mx_features)
     prediction = model.predict(sc_mx_features)
 
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+    df_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
                  8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
                  14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
                  19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
 
-    if prediction[0] in crop_dict:
-        crop = crop_dict[prediction[0]]
+    if prediction[0] in df_dict:
+        crop = df_dict[prediction[0]]
         result = "{} is the best crop to be cultivated right there".format(crop)
     else:
         result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
